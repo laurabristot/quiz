bc735f0b381a3d59f83a05136e9eb67845dea3a1
laura bristot <laurabristotbarbi@hotmail.com>
2023-10-09 16:35:42 -0300

    #9 handling new answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,23 @@
-export default function Options({ question }) {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer !== null
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option">{option}</button>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+            hasAnswer
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }`}
+          key={option}
+          disabled={hasAnswer}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+        >
+          {option}
+        </button>
       ))}
     </div>
   )
