cf44cd1e29af3a9f6e54d7620174120f7b1d264e
Laura Bristot <107277969+laurabristot@users.noreply.github.com>
2023-10-09 17:32:38 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "./index.css";
-<<<<<<< HEAD
-import DateCounter from "./DateCounter";
-=======
-<<<<<<< HEAD
-import DateCounter from "./DateCounter";
-=======
->>>>>>> a7d7746bb5ac55e30ca994a723a413bcaf2398b3
->>>>>>> a9698ad2bf5310a52ea9a715efc5f793a2d24a68
+import Header from "./Header";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <main className='main'> 
+        
+      </main>
     </div>
   );
 }
